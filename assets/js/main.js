cb172298712b87157c1849f2cc8e58701ab81f84
Ricardo Valdes <ricardovaldes@everfall-games.com>
2022-01-23 13:41:22 -0500

    Third Commit - @ 25mins, Added js functionality.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,21 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
+const navMenu = document.getElementById('nav-Menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
 
-
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 
